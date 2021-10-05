fc2cbae8c1454a3296a940cc050898eedc1a8ace
datayang <ceo@mail.datayang.com>
2021-10-05 20:19:22 +0800

    tauri整合vue3和element ui

---
+++ tauri-vue-element-ui/README.md
@@ -0,0 +1,4 @@
+# tauri-vue-element-ui
+
+此项目整合tauri+vue3+element ui
+
---
+++ tauri-vue-element-ui/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <img src="./assets/logo.png">
+  <div>
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-button type="primary">el-button</el-button>
+
+    <el-button type="text" @click="open">点击打开 Message Box</el-button>
+
+  </div>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+      open() {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
